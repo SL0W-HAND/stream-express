18746ea97344abc001b5f85514eb1c4f8abd9b98
Daniel <DanielCarrete_Dev@outlook.com>
2021-02-15 22:36:21 -0600

    read folder

--- folderPath.js
+++ folderPath.js
@@ -1,3 +1,3 @@
 //Put here the path of the folder with the videos
-const folderPath = 'C:\Users\86494\Videos';
-export default folderPath;
+const folderPath = 'C:/Users/86494/Desktop/vid';
+module.exports = folderPath
