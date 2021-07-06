cc364e8373366b8438ccadacd4287b8341ba335a
Daniel <DanielCarrete_Dev@outlook.com>
2021-07-06 17:55:25 -0500

    adding login

---
+++ videoAPI/database/db.ts
@@ -0,0 +1,39 @@
+// This is not a real database,
+// But let's imagine it is one :)
+import allData from './data'
+
+interface TypeVideo {
+    id: number,
+    name: string,
+    duration: number
+}
+
+class Database {
+    constructor() {}
+    
+    async getAll():Promise<TypeVideo[]> {
+        const asArray:TypeVideo[] = Object.values(allData)
+        return asArray
+    }
+
+    async getById(id: string): Promise<TypeVideo | null> {
+        if (!Object.prototype.hasOwnProperty.call(allData, id)) {
+          return null
+        }
+        const entry = allData[id]
+        await randomDelay()
+        return entry
+    }
+}
+
+// Let's also add a delay to make it a bit closer to reality
+const randomDelay = () =>
+  new Promise((resolve) => {
+    const max = 350
+    const min = 100
+    const delay = Math.floor(Math.random() * (max - min + 1)) + min
+
+    setTimeout(resolve, delay)
+})
+
+  export default Database
